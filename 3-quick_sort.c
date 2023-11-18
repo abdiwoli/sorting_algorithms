d4b030ec652d59032e7ed65a4fcabf42e3a8f18b
Muna-Saeed <munasaeed6367@gmail.com>
2023-11-18 07:40:24 -0800

    3. Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,9 +7,9 @@
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
@@ -23,30 +23,30 @@ void swap(int *a, int *b)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1;
-    int j;
+	int pivot = array[high];
+	int i = low - 1;
+	int j;
 
-    for (j = low; j < high; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            if (i != j)
-            {
-                swap(&array[i], &array[j]);
-                print_array(array, size);
-            }
-        }
-    }
+	for (j = low; j < high; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
+		}
+	}
 
-    if (i + 1 != high)
-    {
-        swap(&array[i + 1], &array[high]);
-        print_array(array, size);
-    }
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
 
-    return (i + 1);
+	return (i + 1);
 }
 
 /**
@@ -58,13 +58,13 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  */
 void quicksort_recursive(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot = lomuto_partition(array, low, high, size);
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
 
-        quicksort_recursive(array, low, pivot - 1, size);
-        quicksort_recursive(array, pivot + 1, high, size);
-    }
+		quicksort_recursive(array, low, pivot - 1, size);
+		quicksort_recursive(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -74,8 +74,8 @@ void quicksort_recursive(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    quicksort_recursive(array, 0, size - 1, size);
+	quicksort_recursive(array, 0, size - 1, size);
 }
